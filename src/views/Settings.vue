d916e69197bf9dc3591d3d72fee3c924eff123f1
ElliotThiebaut <elliotthiebaut1@gmail.com>
2022-06-23 22:54:59 +0200

    :sparkles: Added version number in settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,13 +2,17 @@
     <div class="responsive-container">
         <h1 class="text-black text-4xl font-title font-bold py-20 text-center dark:text-white">Settings</h1>
 
-        <div class="flex w-full justify-between">
-            <p>Dark mode</p>
-            <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
-                <input @click="settingsStore.toggleDarkMode(!settingsStore.darkMode)" type="checkbox" value="" id="default-toggle" class="sr-only peer" ref="darkModeToggle">
-                <div class="w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            </label>
+        <div class="min-h-[55vh]">
+            <div class="flex w-full justify-between">
+                <p>Dark mode</p>
+                <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
+                    <input @click="settingsStore.toggleDarkMode(!settingsStore.darkMode)" type="checkbox" value="" id="default-toggle" class="sr-only peer" ref="darkModeToggle">
+                    <div class="w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+            </div>
         </div>
+
+        <p class="text-center mt-10 text-gray-500 font-title font-bold">Version {{ version }}</p>
     </div>
 </template>
 
@@ -18,6 +22,7 @@ import {onMounted, ref} from "vue";
 
 const settingsStore = useSettingsStore()
 const darkModeToggle = ref(null)
+const version = __APP_VERSION__
 
 onMounted(() => {
     darkModeToggle.value.checked = !!settingsStore.darkMode;
